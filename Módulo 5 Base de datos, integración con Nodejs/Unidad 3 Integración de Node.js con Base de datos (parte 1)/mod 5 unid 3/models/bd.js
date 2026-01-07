71b48ce5ed96a3e1e00d54d4315e16f2ebcb2b96
Bistotto David <dhbistotto@gmail.com>
2026-01-07 18:08:14 -0300

    Mod 5 Unid 3 Integración de Node c Base de datos Tarea

---
+++ Módulo 5 Base de datos, integración con Nodejs/Unidad 3 Integración de Node.js con Base de datos (parte 1)/mod 5 unid 3/models/bd.js
@@ -0,0 +1,15 @@
+var mysql = require('mysql2');
+var util = require('util');
+
+var pool  = mysql.createPool({
+  connectionLimit : 10,
+  host            : process.env.mysql_host,
+  user            : process.env.mysql_user,
+  password        : process.env.mysql_password,
+  database        : process.env.mysql_db_name
+});
+
+
+pool.query = util.promisify(pool.query);
+
+module.exports = pool;
